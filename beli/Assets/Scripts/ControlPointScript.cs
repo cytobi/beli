238544df8b0bb08256eb925ca5ef1a13a22f0373
cytobi <tobi81548@gmail.com>
2023-07-11 20:01:33 +0200

    make control points clickable

---
+++ beli/Assets/Scripts/ControlPointScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPointScript : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Debug.Log("Clicked control point");
+    }
+}
